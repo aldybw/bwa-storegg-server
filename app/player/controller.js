18608987c073b0f9c78d946cba1c25925e00d583
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-10-01 15:29:44 +0700

    api category

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -28,6 +29,14 @@ module.exports = {
       return res.status(200).json({ data: voucher });
     } catch (err) {
       res.status(500).json({ message: err.message } || `Internal Server Error`);
+    }
+  },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+      res.status(200).json({ data: category });
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal Server Error` });
     }
   },
 };
