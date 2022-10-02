f34e45862a8824c0f910b6a291660955eae4c820
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-10-02 09:31:34 +0700

    api detail transaksi

--- app/player/controller.js
+++ app/player/controller.js
@@ -144,7 +144,19 @@ module.exports = {
         data: history,
         total: total.length ? total[0].value : 0,
       });
-    } catch (er) {
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal Server Error` });
+    }
+  },
+  historyDetail: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const history = await Transaction.findOne({ _id: id });
+      if (!history)
+        return res.status(404).json({ message: "history tidak ditemukan" });
+
+      res.status(200).json({ data: history });
+    } catch (err) {
       res.status(500).json({ message: err.message || `Internal Server Error` });
     }
   },
