4f05cb02c2070b271eacc45cb4bb1be0eaf6e743
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-10-02 11:44:09 +0700

    api read profile

--- app/player/controller.js
+++ app/player/controller.js
@@ -195,4 +195,19 @@ module.exports = {
       res.status(500).json({ message: err.message || `Internal Server Error` });
     }
   },
+  profile: async (req, res) => {
+    try {
+      const player = {
+        id: req.player._id,
+        username: req.player.username,
+        email: req.player.email,
+        name: req.player.name,
+        avatar: req.player.avatar,
+        phoneNumber: req.player.phoneNumber,
+      };
+      res.status(200).json({ data: player });
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal Server Error` });
+    }
+  },
 };
